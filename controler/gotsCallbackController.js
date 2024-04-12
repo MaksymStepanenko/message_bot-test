cf812ad8d9b7c9043d1c775dff9ad77b129eab79
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2024-04-12 12:00:25 +0300

    coment all data

--- controler/gotsCallbackController.js
+++ controler/gotsCallbackController.js
@@ -10,9 +10,9 @@ const liqpay = new LiqPay(public_key, private_key);
 const gotsCallbackController = async (req, res) => {
   const { data, signature } = req.body;
   console.log("req.body:", req.body);
-  console.log("Received data:", data);
+  // console.log("Received data:", data);
   // Розкодування параметра data з base64 та перетворення його в рядок JSON
-  const decodedData = Buffer.from(data, "base64").toString("utf-8");
+  // const decodedData = Buffer.from(data, "base64").toString("utf-8");
 
   // Створення підпису на вашому сервері
   const calculatedSignature = crypto
@@ -25,12 +25,12 @@ const gotsCallbackController = async (req, res) => {
     // Якщо підписи співпадають, то запит від LiqPay є справжнім
     // Тут ви можете обробити запит відповідно до ваших потреб
     console.log("Справжня відповідь від сервера LiqPay");
-    console.log(decodedData);
+    // console.log(decodedData);
     // Наприклад, ви можете виконати додаткові дії згідно з отриманим статусом платежу
   } else {
     // Якщо підписи не співпадають, запит може бути підробленим
     console.log("Недійсний запит від сервера LiqPay");
-    console.log(decodedData);
+    // console.log(decodedData);
   }
 
   res.end(); // Завершення відповіді
