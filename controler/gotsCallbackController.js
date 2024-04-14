fb44d1316a2ca1a2aa18dba9d08194ef01147d71
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2024-04-14 13:19:51 +0300

    14,8

--- controler/gotsCallbackController.js
+++ controler/gotsCallbackController.js
@@ -9,28 +9,47 @@ const liqpay = new LiqPay(public_key, private_key);
 
 const gotsCallbackController = async (req, res) => {
   const { data, signature } = req.body;
-  // console.log("Received data:", data);
-  // Розкодування параметра data з base64 та перетворення його в рядок JSON
-  // const decodedData = Buffer.from(data, "base64").toString("utf-8");
-  // Створення підпису на вашому сервері
-  const calculatedSignature = crypto
-    .createHash("sha1")
-    .update(private_key + data + private_key)
-    .digest("base64");
 
-  // Порівняння отриманої підпису з підписом, що надійшов у запиті
-  if (signature === calculatedSignature) {
+  // Розрахунок підпису
+  const sign = liqpay.str_to_sign(private_key + data + private_key);
+
+  // Порівняння отриманого підпису з підписом, що надійшов у запиті
+  if (signature === sign) {
     // Якщо підписи співпадають, то запит від LiqPay є справжнім
-    // Тут ви можете обробити запит відповідно до ваших потреб
     console.log("Справжня відповідь від сервера LiqPay");
-    console.log(req.body);
-    // Наприклад, ви можете виконати додаткові дії згідно з отриманим статусом платежу
+    console.log("Data:", data);
+    // Обробка запиту відповідно до ваших потреб
   } else {
     // Якщо підписи не співпадають, запит може бути підробленим
     console.log("Недійсний запит від сервера LiqPay");
-    console.log(req.body);
+    console.log("Data:", data);
   }
+
   res.end(); // Завершення відповіді
 };
 
 module.exports = gotsCallbackController;
+
+// const { data, signature } = req.body;
+  // // console.log("Received data:", data);
+  // // Розкодування параметра data з base64 та перетворення його в рядок JSON
+  // // const decodedData = Buffer.from(data, "base64").toString("utf-8");
+  // // Створення підпису на вашому сервері
+  // const calculatedSignature = crypto
+  //   .createHash("sha1")
+  //   .update(private_key + data + private_key)
+  //   .digest("base64");
+
+  // // Порівняння отриманої підпису з підписом, що надійшов у запиті
+  // if (signature === calculatedSignature) {
+  //   // Якщо підписи співпадають, то запит від LiqPay є справжнім
+  //   // Тут ви можете обробити запит відповідно до ваших потреб
+  //   console.log("Справжня відповідь від сервера LiqPay");
+  //   console.log(req.body);
+  //   // Наприклад, ви можете виконати додаткові дії згідно з отриманим статусом платежу
+  // } else {
+  //   // Якщо підписи не співпадають, запит може бути підробленим
+  //   console.log("Недійсний запит від сервера LiqPay");
+  //   console.log(req.body);
+  // }
+  // res.end(); // Завершення відповіді
