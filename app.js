8962e2a5af4f1d00e55410edab821fa19b94e200
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2024-04-14 11:22:18 +0300

    14/1

--- app.js
+++ app.js
@@ -23,17 +23,16 @@ app.use(express.json());
 app.post("/liqpay-gots", liqpayController);
 app.post("/get-current-status", (req, res) => {
   const order_id = req.body.order_id;
+  console.log(order_id);
   liqpay.api(
     "request",
     {
-      public_key: public_key,
       action: "status",
       version: "3",
       order_id: order_id,
     },
     function (json) {
       console.log(json.status);
-      res.json(json);
     }
   );
 });
