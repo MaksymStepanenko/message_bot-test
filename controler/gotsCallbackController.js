c5533ba0b2d1e9c5dd3e3cc2fdaa2e6b6a864b80
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2024-04-14 18:46:37 +0300

    14/05

--- controler/gotsCallbackController.js
+++ controler/gotsCallbackController.js
@@ -16,30 +16,10 @@ const gotsCallbackController = async (req, res) => {
   });
 
   req.on("end", () => {
-    const { data, signature } = req.body;
+    // const { data, signature } = req.body;
 
-    // Розкодувати дані з base64
-    const decodedData = Buffer.from(data, "base64").toString("utf-8");
-
-    // Розрахунок підпису
-    const sign = liqpay.str_to_sign(private_key + data + private_key);
-
-    // Порівняння отриманого підпису з підписом, що надійшов у запиті
-    if (signature === sign) {
-      // Якщо підписи співпадають, то запит від LiqPay є справжнім
-      console.log("Справжня відповідь від сервера LiqPay");
-      console.log("Data:", decodedData);
-      // Обробка запиту відповідно до ваших потреб
-    } else {
-      // Якщо підписи не співпадають, запит може бути підробленим
-      console.log("Недійсний запит від сервера LiqPay");
-      console.log("Data:", decodedData);
-    }
-
-    res.end(); // Завершення відповіді
-    // // Розкодувати дані з тіла запиту
-    // const decodedData = querystring.parse(body);
-    // const { data, signature } = decodedData;
+    // // Розкодувати дані з base64
+    // const decodedData = Buffer.from(data, "base64").toString("utf-8");
 
     // // Розрахунок підпису
     // const sign = liqpay.str_to_sign(private_key + data + private_key);
@@ -57,6 +37,27 @@ const gotsCallbackController = async (req, res) => {
     // }
 
     // res.end(); // Завершення відповіді
+    // Розкодувати дані з тіла запиту
+    const decodedData = querystring.parse(body);
+    const { data, signature } = decodedData;
+
+    const baseDecodedData = Buffer.from(data, "base64").toString("utf-8");
+    // Розрахунок підпису
+    const sign = liqpay.str_to_sign(private_key + data + private_key);
+
+    // Порівняння отриманого підпису з підписом, що надійшов у запиті
+    if (signature === sign) {
+      // Якщо підписи співпадають, то запит від LiqPay є справжнім
+      console.log("Справжня відповідь від сервера LiqPay");
+      console.log("Data:", baseDecodedData);
+      // Обробка запиту відповідно до ваших потреб
+    } else {
+      // Якщо підписи не співпадають, запит може бути підробленим
+      console.log("Недійсний запит від сервера LiqPay");
+      console.log("Data:", baseDecodedData);
+    }
+
+    res.end(); // Завершення відповіді
   });
 };
 
